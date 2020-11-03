f8f2b1bd1e5125f58e2ed9fdabc04c482e977e74
Daniel Oyagha <daniel.oyagha@gmail.com>
2020-11-03 09:30:25 +0100

    new

--- script.js
+++ script.js
@@ -67,7 +67,7 @@ function stopWatch(){
 
 function startStop(){
   if (status === "stopped"){
-    interval = window.setInterval(stopWatch, 100);
+    interval = window.setInterval(stopWatch, 1000);
     document.getElementById("startStop").innerHTML = "Stop";
     status= "started";
   }
